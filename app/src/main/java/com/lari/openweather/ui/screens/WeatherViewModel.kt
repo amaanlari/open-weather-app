a999d9c91a40afc570b01340840e21949a086838
amaanlari <amaanlari@outlook.comm>
2025-03-20 11:23:44 +0530

    feat: Implement Weather data fetching via API
    - Added `WeatherResponse` data class to model weather data.
    - Created `WeatherApi` interface with Retrofit to fetch weather from OpenWeather API.
    - Implemented `WeatherViewModel` to manage weather data and fetch it using `WeatherApi`.

---
+++ app/src/main/java/com/lari/openweather/ui/screens/WeatherViewModel.kt
@@ -0,0 +1,27 @@
+package com.lari.openweather.ui.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lari.openweather.api.WeatherApi
+import com.lari.openweather.model.WeatherResponse
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class WeatherViewModel : ViewModel() {
+
+    private val _weatherData = MutableStateFlow<WeatherResponse?>(null)
+    val weatherData: StateFlow<WeatherResponse?> = _weatherData
+    private val _weatherApi = WeatherApi.create()
+
+    fun fetchWeather(city: String, apiKey: String, units: String) {
+        viewModelScope.launch {
+            try {
+                val response = _weatherApi.getWeather(city, apiKey, units)
+                _weatherData.value = response
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
